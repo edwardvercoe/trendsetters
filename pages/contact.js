9b63a23e9a745164fc7ae941c02c68137182fc79
Ed <edward.vercoe@gmail.com>
2022-03-07 14:04:05 +1100

    testing form

---
+++ pages/contact.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Container from "@/components/Container";
+import Section from "@/components/Section";
+import IndividualForm from "@/components/forms/IndividualForm";
+import styled from "styled-components";
+
+import Hero from "@/components/Hero";
+
+import { TextInput } from "@mantine/core";
+
+import TestForm from "@/components/forms/TestForm";
+import AnotherTest from "@/components/forms/AnotherTest";
+
+const Contact = () => {
+  const [submitterName, setSubmitterName] = useState("");
+
+  return (
+    <main>
+      <Hero />
+      <Section>
+        <Container>
+          <Heading>TRENDsetters nomination form</Heading>
+          <InnerContainer>
+            {/* <TextInput placeholder="Your name" value={submitterName} onChange={setSubmitterName} /> */}
+
+            {/* <IndividualForm /> */}
+
+            <TestForm />
+
+            {/* <AnotherTest /> */}
+          </InnerContainer>
+        </Container>
+      </Section>
+    </main>
+  );
+};
+
+const Heading = styled.h1`
+  text-align: center;
+`;
+
+const InnerContainer = styled.div`
+  max-width: 768px;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export default Contact;
