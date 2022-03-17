6694be3c0bae21d6a8caadd9250aa741ef151d5b
Ed <edward.vercoe@gmail.com>
2022-03-18 10:28:46 +1100

    nav fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 import Image from "next/image";
 
 import trendlogo from "@/assets/navtrendlogo.png";
-import navbackground from "@/assets/navbg.png";
+import navbackground from "@/assets/navbackground.png";
+
 import bp from "@/styles/breakpoints";
 
 export default function Navbar() {
